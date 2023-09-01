6f09c7364f7acc3604c71e00f6eb7381dd9e6375
Nathan Heaps <nsheaps@gmail.com>
2023-08-31 23:59:34 -0400

    fix formula

--- Formula/devsetup-base.rb
+++ Formula/devsetup-base.rb
@@ -1,7 +1,12 @@
 class DevsetupBase < Formula
   desc 'A tool to setup a new development environment'
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
+  url 'https://raw.githubusercontent.com/nsheaps/homebrew-devsetup/main/Formula/devsetup-base.rb'
+  version '0.0.1' # bump me if you want people to re-install these things.
 
+  # if these are installed from this tap, then devsetup upgrade-all will upgrade them
+  # if you want to reference another formula in another tap here so that it _will_ get upgraded, use
+  #   devsetup alias <formula>
   # depends_on "git"
   # depends_on "python3"
   # depends_on "ansible"

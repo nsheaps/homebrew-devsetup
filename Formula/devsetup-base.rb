91afba0065e029099691b9028902591a914e810d
Nathan Heaps <nsheaps@gmail.com>
2023-08-31 23:42:22 -0400

    push first formula

---
+++ Formula/devsetup-base.rb
@@ -0,0 +1,18 @@
+class DevsetupBase < Formula
+    desc "A tool to setup a new development environment"
+    homepage "http://github.com/nsheaps/homebrew-devsetup"
+
+    # depends_on "git"
+    # depends_on "python3"
+    # depends_on "ansible"
+
+    # depends_on "nsheaps/devsetup/devsetup-bin"
+
+    def install
+        system "echo", "install"
+    end
+
+    def uninstall
+        super
+        echo "goodbye cruel world"
+    end

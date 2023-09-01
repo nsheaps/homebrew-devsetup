8dbe6ff6932ee1bb65d1d1668c9c80bde433d199
Nathan Heaps <nsheaps@gmail.com>
2023-09-01 00:02:22 -0400

    more stuff

--- Formula/devsetup-base.rb
+++ Formula/devsetup-base.rb
@@ -2,7 +2,12 @@ class DevsetupBase < Formula
   desc 'A tool to setup a new development environment'
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://raw.githubusercontent.com/nsheaps/homebrew-devsetup/main/Formula/devsetup-base.rb'
-  version '0.0.1' # bump me if you want people to re-install these things.
+  version '0.0.1' # bump me if you want people to re-install these things, like if the list changed.
+  sha256 "18178d02a85eec00bf87b5328dbc20582a4c07b414669687b18a4997e6786b37"
+
+  livecheck do
+    url :stable
+  end
 
   # if these are installed from this tap, then devsetup upgrade-all will upgrade them
   # if you want to reference another formula in another tap here so that it _will_ get upgraded, use
@@ -14,6 +19,7 @@ class DevsetupBase < Formula
   # depends_on "nsheaps/devsetup/devsetup-bin"
 
   def install
+    run 'echo', 'installllllio'
     system 'echo', 'install'
   end
 

b3f9416afdab08ae4c9a901892129dfdb54780bd
Nathan Heaps <nsheaps@gmail.com>
2023-09-07 01:34:37 -0400

    fix

--- Formula/devsetup-base.rb
+++ Formula/devsetup-base.rb
@@ -2,7 +2,8 @@ class DevsetupBase < Formula
   desc 'A tool to setup a new development environment'
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://github.com/nsheaps/brew-meta-formula/archive/refs/tags/v1.0.0.tar.gz'
-  version '0.0.1' # bump me if you want people to re-install these things, like if the list changed.
+  sha256 "b14702dd54ea5c48d2ebeb6425015c14794159a6b9d342178c81d2f2e79ed2db"
+  version '0.0.2' # bump me if you want people to re-install these things, like if the list changed.
 
   # if these are installed from this tap, then devsetup upgrade-all will upgrade them
   # if you want to reference another formula in another tap here so that it _will_ get upgraded, use
@@ -14,6 +15,8 @@ class DevsetupBase < Formula
   # depends_on "nsheaps/devsetup/devsetup-bin"
 
   def install
+    puts 'before install'
+    super
     puts 'install'
 
   end

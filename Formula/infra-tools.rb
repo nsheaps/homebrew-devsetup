4e4b2582ba43f3c6e9cd31abd4148281eea34d8f
Nathan Heaps <1282393+nsheaps@users.noreply.github.com>
2024-03-20 10:35:21 -0400

    use tgenv instead of terragrunt

--- Formula/infra-tools.rb
+++ Formula/infra-tools.rb
@@ -3,7 +3,7 @@ class InfraTools < Formula
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://github.com/nsheaps/brew-meta-formula/archive/refs/tags/v1.0.0.tar.gz'
   sha256 "b14702dd54ea5c48d2ebeb6425015c14794159a6b9d342178c81d2f2e79ed2db"
-  version '1.0.0' # bump me if you want people to re-install these things, like if the list changed.
+  version '1.0.1' # bump me if you want people to re-install these things, like if the list changed.
 
   livecheck do
     skip "Meta formulas cannot be updated"
@@ -14,8 +14,8 @@ class InfraTools < Formula
 
   # if you want to reference another formula in another tap here so that it _will_ get
   # upgraded, use `devsetup alias <formula>`, then reference it with nsheaps/devsetup/<original>
-  depends_on "tfenv"
-  depends_on "terragrunt"
+  depends_on "tfenv" # instead of terraform
+  depends_on "tgenv" # instead of terragrunt
   depends_on "kubectl"
   depends_on "k2tf"
   depends_on "helm"

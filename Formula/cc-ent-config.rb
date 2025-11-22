183bf2b268ec61f7b6dddaaf081fbf3c95cf577b
Nathan Heaps <1282393+nsheaps@users.noreply.github.com>
2025-11-21 20:25:24 -0500

    Create cc-ent-config.rb

---
+++ Formula/cc-ent-config.rb
@@ -0,0 +1,2 @@
+# a formula that when installed will tale a heredoc defined in this file and place it in the appropriate location on a mac machine
+# if modifying user configs, use jsonnet to merge the items with the existing config

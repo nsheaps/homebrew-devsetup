bdd28073828a9d7c34fe355fb0328e99c31a27c6
Nathan Heaps <1282393+nsheaps@users.noreply.github.com>
2025-11-28 19:00:46 -0600

    Bump version to 1.0.5 in nsheaps-base.rb

--- Casks/nsheaps-base.rb
+++ Casks/nsheaps-base.rb
@@ -4,7 +4,7 @@ cask "nsheaps-base" do
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://github.com/nsheaps/brew-meta-formula/archive/refs/tags/v1.0.0.tar.gz'
   sha256 "b14702dd54ea5c48d2ebeb6425015c14794159a6b9d342178c81d2f2e79ed2db"
-  version '1.0.4' # bump me if you want people to re-install these things, like if the list changed.
+  version '1.0.5' # bump me if you want people to re-install these things, like if the list changed.
   ### WHAT IS THIS
   # Running the quick start script will:
   # - install homebrew if not already installed
@@ -111,6 +111,7 @@ cask "nsheaps-base" do
   depends_on cask: "claude-code"
   depends_on cask: "visual-studio-code"
   depends_on cask: "google-chrome"
+  depends_on cask: "karabiner-elements"
   depends_on cask: "slack"
   depends_on cask: "notion"
   depends_on cask: "linear-linear"

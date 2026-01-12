f6ff5fb5d20016fd9d2fc5ff97451d896c2ff3cf
Nathan Heaps <1282393+nsheaps@users.noreply.github.com>
2026-01-11 21:28:22 -0500

    fix: make it on post install

--- Casks/nsheaps-base.rb
+++ Casks/nsheaps-base.rb
@@ -4,7 +4,7 @@ cask 'nsheaps-base' do
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://github.com/nsheaps/brew-meta-formula/archive/refs/tags/v1.0.0.tar.gz'
   sha256 'b14702dd54ea5c48d2ebeb6425015c14794159a6b9d342178c81d2f2e79ed2db'
-  version '1.0.9' # bump me if you want people to re-install these things, like if the list changed.
+  version '1.0.10' # bump me if you want people to re-install these things, like if the list changed.
   ### WHAT IS THIS
   # Running the quick start script will:
   # - install homebrew if not already installed
@@ -133,21 +133,22 @@ cask 'nsheaps-base' do
     # complain about an empty installation
     system 'touch', 'trick-brew-to-install-meta-formula'
     prefix.install 'trick-brew-to-install-meta-formula'
+  end
 
+  def post_install
     # notify the user that we're about to install stuff from the app store
-     "Now installing apps from the mac app store using 'mas'. You may be prompted to log in."
     # pass a heredoc to brew install as an in-line Brewfile in order to install apps from the mac store
     # using the following syntax:
+    ohai "Installing mac app store apps..."
 
-      ohai "Installing mac app store apps..."
+    system "brew", "bundle", "--file=/dev/stdin" , :input => <<~BUNDLE
+      brew "mas"
+      
+      mas "Spokenly", id: 6740315592 # Speech To Text
+    BUNDLE
 
-      system "brew", "bundle", "--file=/dev/stdin" , :input => <<~BUNDLE
-        brew "mas"
-        
-        mas "Spokenly", id: 6740315592 # Speech To Text
-      BUNDLE
+    ohai "Finished installing mac app store apps."
 
-      ohai "Finished installing mac app store apps."
   end
 
   def caveats

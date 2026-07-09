87cc3358c4bf8ea0591642b959a57f6537eb4eed
Nathan Heaps (@Oura) <300890611+nsheaps-oura@users.noreply.github.com>
2026-07-09 16:18:31 -0400

    update caveats

--- Casks/nsheaps-base.rb
+++ Casks/nsheaps-base.rb
@@ -4,7 +4,7 @@ cask 'nsheaps-base' do
   homepage 'http://github.com/nsheaps/homebrew-devsetup'
   url 'https://github.com/nsheaps/brew-meta-formula/archive/refs/tags/v1.0.0.tar.gz'
   sha256 'b14702dd54ea5c48d2ebeb6425015c14794159a6b9d342178c81d2f2e79ed2db'
-  version '1.0.24' # bump me if you want people to re-install these things, like if the list changed.
+  version '1.0.26' # bump me if you want people to re-install these things, like if the list changed.
   ### WHAT IS THIS
   # Running the quick start script will:
   # - install homebrew if not already installed
@@ -25,9 +25,6 @@ cask 'nsheaps-base' do
   # - manage anything other than brew installs for casks and formulae
   # - work on linux (casks are not supported)
   ### QUICKSTART
-  # Prerequisites:
-  # - Don't have any apps installed already which will be installed by casks
-  # - must be on macos
   #
   # Dry run:
   # brew update && brew outdated
@@ -42,45 +39,9 @@ cask 'nsheaps-base' do
   # FETCH=($(command -v curl &>/dev/null && echo 'curl -fsSL' || echo 'wget -O -'))
   # bash <($FETCH "https://raw.githubusercontent.com/nsheaps/homebrew-devsetup/HEAD/install_brew.sh")
 
-  # \. ~/.zshrc
-  # brew update && brew trust nsheaps/devsetup && brew install --cask --adopt nsheaps/devsetup/nsheaps-base
+  # then follow the instructions in the CAVEATS section 
 
-  # cat << 'EOF' >> ~/.zshrc
-
-  # setopt interactivecomments
-
-  # source /opt/homebrew/opt/antidote/share/antidote/antidote.zsh
-
-  # # Initialize antidote's dynamic mode, which changes `antidote bundle`
-  # # from static mode.
-  # source <(antidote init)
-
-  # antidote bundle <<EOBUNDLES
-  #     zsh-users/zsh-autosuggestions
-  #     zsh-users/zsh-completions
-  #     getantidote/use-omz
-  #     ohmyzsh/ohmyzsh path:lib
-  #     ohmyzsh/ohmyzsh path:plugins/git
-  #     ohmyzsh/ohmyzsh path:plugins/autojump
-  #     ohmyzsh/ohmyzsh path:plugins/brew
-  #     ohmyzsh/ohmyzsh path:plugins/direnv
-  #     ohmyzsh/ohmyzsh path:plugins/docker
-  #     ohmyzsh/ohmyzsh path:plugins/mise
-  #     ohmyzsh/ohmyzsh path:plugins/thefuck
-  #     ohmyzsh/ohmyzsh path:plugins/command-not-found
-  #     ohmyzsh/ohmyzsh path:themes/robbyrussell.zsh-theme
-  # EOBUNDLES
-
-  # EOF
-
-  # \. ~/.zshrc
-
-  # mise use -g \
-  #   node@lts \
-  #   bun \
-  #   python \
-  #   golang
-  # mise ls
+  # =====
 
   # stage_only true
 
@@ -128,12 +89,12 @@ cask 'nsheaps-base' do
   depends_on cask: '1password-cli'
   depends_on cask: 'alfred' # spotlight replacement
   depends_on cask: 'loop' # window tiling manager
-  depends_on cask: 'claude' # claude desktop
+  # depends_on cask: 'claude' # claude desktop
   depends_on cask: 'claude-code'
   depends_on cask: 'visual-studio-code'
   depends_on cask: 'google-chrome'
   depends_on cask: 'karabiner-elements' # keyboard customizer
-  depends_on cask: 'slack'
+  # depends_on cask: 'slack'
   # depends_on cask: 'notion'
   depends_on cask: 'linear'
   depends_on cask: 'jordanbaird-ice' # menu tray organizer
@@ -185,34 +146,48 @@ cask 'nsheaps-base' do
           ohmyzsh/ohmyzsh path:themes/robbyrussell.zsh-theme
       EOBUNDLES
 
+      alias brew-outdated="brew update && brew outdated"
+      alias brew-update-all="brew update && brew upgrade --greedy"
+
       EOF
 
       # re-source the rc file to load the plugins
       . ~/.zshrc
-
-      gum spin --show-output --title "installing node@lts using mise" -- mise use -g node@lts
-      gum spin --show-output --title "installing bun using mise"      -- mise use -g bun
-      gum spin --show-output --title "installing python using mise".  -- mise use -g python
-      gum spin --show-output --title "installing golang using mise"   -- mise use -g golang
-      gum style '$ mise ls' '' "$(mise ls)"
+      gum log --structured --level debug "installing global tools using mise"
+      gum spin --show-output --title "installing global tools using mise" -- mise use --raw -g \
+        node@lts \
+        bun \
+        python \
+        golang
+      
+      gum log --structured --level info "installed tools with mise" "mise-ls" "$(mise ls)"
 
       # install rosetta compatibility layer
-      gum spin --show-output --title "installing rosetta compatibility layer" -- softwareupdate --install-rosetta
+      gum log --structured --level debug "installing rosetta compatibility layer"
+      { gum spin --show-output --title "installing rosetta compatibility layer" -- softwareupdate --install-rosetta --agree-to-license ; } && gum log --structured --level info "Installed rosetta"
 
       # login to `gh`
-      gum spin --show-output --title "logging into github using gh" -- true
-      gh auth login
+      gum log  --structured --level debug "logging into github with gh"
+      { gh auth login ; } && gum log  --structured --level info "Logged into github"
 
       # set up git to use `gh` as the credential helper
-      gum spin --show-output --title "setting gh as git credential helper" -- gh setup-git
+      gh auth setup-git && gum log --structured --level info "Set gh as git credential helper"
 
       gum style \
+        --foreground 212 --border-foreground 212 --border double \
+        --align left --margin "1 2" --padding "2 4" \
         'Setup is mostly complete. Please do the following:' \
         '  - restart your computer so the rc file gets sourced at the OS level on login' \
-        '  - generate gpg keys and add to github and 1password' \
-        '  - generate ssh keys and add to github and 1password' \
+        '  - generate gpg keys and add to github / 1pass' \
+        '  - generate ssh keys and add to github / 1pass' \
+        '' \
+        'To see what formulae/casks are out of date:' \
+        '  $ brew-outdated' \
+        'To update all taps and their installed formulae/casks:' \
+        '  $ brew-update-all'
 
       ### END COPY HERE
+
     CAVEATS
   end
 end
